75c67e3eefcba7e23db2cc69abea676731d90738
passos222 <148768070+passos222@users.noreply.github.com>
2024-04-03 09:35:12 -0300

    HF

--- main.js
+++ main.js
@@ -14,4 +14,24 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 const contadores = document.querySelectorAll(".contador")
-const tempoObjetivo1 = new Date("2024-03-25T00:00:00")
+const tempoObjetivo1 = new Date("2024-12-10T00:00:00")
+const tempoObjetivo2 = new Date("2025-01-01T00:00:00")
+const tempoObjetivo3 = new Date("2027-08-10T00:00:00")
+const tempoObjetivo4 = new Date("2030-01-01T00:00:00")
+
+const tempos=[tempoObjetivo1;tempoObjetivo2;tempoObjetivo3;tempoObjetivo4;]
+contadores[0].textContent = calculaTempo(tempo0);
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
